1c7e4acf2736580ddbffbee5e1af509e516a7d90
tsarni <tomi.sarni@gmail.com>
2013-01-22 14:00:12 +0200

    -fixed charset to show scandinavian letters correctly

--- SmartFlow/src/org/smartflow/Resources.java
+++ SmartFlow/src/org/smartflow/Resources.java
@@ -8,7 +8,7 @@ public abstract class Resources {
 			"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"/>"	+	
 			"<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js\" type=\"text/javascript\" charset=\"iso-8859-1\"></script>" +
 			"<title>Smart Kitchen Workflow Engine</title>" +
-			"<script src=\"client.js\"  type=\"text/javascript\" charset=\"ISO-8859-1\"></script>" +
+			"<script src=\"client.js\"  type=\"text/javascript\" charset=\"iso-8859-1\"></script>" +
 		    "</head>" +
 			"<body>" +
 		    "<div align=\"center\" id=\"holder\">";
